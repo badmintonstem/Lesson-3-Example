c122464d7b5325f47f026e805383074d7bf5fd7b
badmintonstem <jhope@badmintonschool.co.uk>
2020-02-25 08:03:29 +0000

    flow control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-
-numSum = num1 + num2
-
-print("The sum of your two numbers is... " + numSum)
+potatoes = int(input("How many potatoes? > "))
+if potatoes < 10:
+    print("Buy more potatoes!")
+else:
+    print("We have enough potatoes.")
